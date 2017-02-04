cfcf62161b61197abf85330acf3faca8897b76b6
Mike Lazer-Walker <michael@lazerwalker.com>
2017-02-04 14:09:39 -0500

    Add systemDefaults script

---
+++ systemDefaults.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Show all file extensions
+defaults write -g AppleShowAllExtensions -bool true
+
+# Show hidden files
+defaults write com.apple.finder AppleShowAllFiles true
+
+# Show full path in finder
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Unhide user library
+chflags nohidden ~/Library
+
+# Show 'quit finder' option
+defaults write com.apple.finder QuitMenuItem -bool true && \
+killall Finder
+
+# Expand save panel by defaults
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true && \
+defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Don't create .DS_Store on network or USB drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable keyboard autocorrect
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
