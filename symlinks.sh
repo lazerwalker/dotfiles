154a473b0f509d13d8bb26225db34336c002a1bb
Mike Lazer-Walker <michael@lazerwalker.com>
2017-02-06 12:07:13 -0500

    Add VSCode settings JSON

--- symlinks.sh
+++ symlinks.sh
@@ -1,2 +1,6 @@
 dir=`pwd`
 ln -s "$dir/gitconfig" ~/.gitconfig
+
+ln -s "$dir/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
+ln -s "$dir/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+
