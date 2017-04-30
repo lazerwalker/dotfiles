d65f5ad3e60693683f9d88e6371c1e49b72e91f2
Mike Lazer-Walker <michael@lazerwalker.com>
2017-04-30 13:55:30 +0200

    Move zshrc into dotfiles

--- symlinks.sh
+++ symlinks.sh
@@ -4,3 +4,4 @@ ln -s "$dir/gitconfig" ~/.gitconfig
 ln -s "$dir/vscode/keybindings.json" ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s "$dir/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
 
+ln -s "$dir/zshrc" ~/.zshrc
