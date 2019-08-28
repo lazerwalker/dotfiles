bb34a2a95c4ddb194fcc74c0da33ae398c564f72
Em Lazer-Walker <me@lazerwalker.com>
2019-08-28 16:40:43 -0400

    Fix gpg rvm install

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ brew_apps=(
 brew cask install ${brew_apps[@]}
 
 echo "Installing RVM and the latest Ruby..."
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Installing xcselect and latest Xcode CLI tools"
